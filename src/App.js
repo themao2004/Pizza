f3d6fcdcf4b0455e122992d877ce7d2033a5319c
TheSon <buitheson2004@gmail.com>
2024-09-27 11:25:52 +0700

    commit
    # Conflicts:
    #	README.md

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderComponent from './components/HeaderComponent';
+import BannerComponent from './components/Banner';
+import MenuComponent from './components/MenuCompontent';
+import FormComponent from './components/Form';
+
+function App() {
+  return (
+    <>
+
+    <HeaderComponent></HeaderComponent>
+    
+    <BannerComponent></BannerComponent>
+
+    <MenuComponent></MenuComponent>
+
+    <FormComponent></FormComponent>
+    </>
+  );
+}
+
+export default App;
